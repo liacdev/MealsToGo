af30893ddb1667a114a1c4741b818dc87e54434e
Jay For Short <jagoblacksky@gmail.com>
2021-06-22 02:27:40 -0500

    Lesson 143 Tab Navigation

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,14 +1,11 @@
 import React from "react";
-import { StatusBar, FlatList, StyleSheet, SafeAreaView } from "react-native";
 import { Searchbar } from "react-native-paper";
+import { FlatList, StyleSheet } from "react-native";
 import styled from "styled-components/native";
 
+import { SafeArea } from "../../../components/utility//safe-area.component";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
-
-const SafeArea = styled(SafeAreaView)`
-  flex: 1;
-  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
-`;
+import { Spacer } from "../../../components/spacer/spacer.component";
 
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
@@ -46,20 +43,3 @@ export const RestaurantsScreen = () => (
     />
   </SafeArea>
 );
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-
-  search: {
-    padding: 16,
-  },
-
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: "blue",
-  },
-});
