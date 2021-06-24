e90c228a29913f4638c5ffb5f3ba75eaf118fdae
Jay For Short <jagoblacksky@gmail.com>
2021-06-24 13:08:18 -0500

    Lesson 152 Transform data - camelize

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,13 +1,33 @@
 import { mocks } from "./mock";
+import camelize from "camelize";
+
 export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
   return new Promise((resolve, reject) => {
-      const mock = mocks[location];
-      if (!mock) reject("not found");
-        resolve(mock);
-    });
+    const mock = mocks[location];
+    if (!mock) {
+      reject("not found");
+    }
+    resolve(mock);
+  });
 };
-restaurantsRequest().then((result) => {
-    console.log(result);
-}).catch((err) => {
+
+const restaurantsTransform = ({ results = [] }) => {
+  const mappedResults = results.map((restaurant) => {
+    return {
+      ...restaurant,
+      isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
+      isClosedTemporarily: restaurant.business_status === "CLOSED_TEMPORARILY",
+    };
+  });
+  console.log(mappedResults);
+
+  return camelize(mappedResults);
+};
+restaurantsRequest()
+  .then(restaurantsTransform)
+  .then((transformedResponse) => {
+    console.log(transformedResponse);
+  })
+  .catch((err) => {
     console.log("error");
-});
+  });
