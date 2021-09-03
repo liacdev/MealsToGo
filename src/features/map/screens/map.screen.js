bc6aefeda26f82947f573b9f4bb04dde63cd7bef
Jay For Short <jagoblacksky@gmail.com>
2021-09-03 02:29:50 -0500

    186 Linking a Callout to Details

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -14,7 +14,7 @@ const Map = styled(MapView)`
   width: 100%;
 `;
 
-export const MapScreen = () => {
+export const MapScreen = ({ navigation }) => {
   const { location } = useContext(LocationContext);
   const { restaurants = [] } = useContext(RestaurantsContext);
 
@@ -50,7 +50,13 @@ export const MapScreen = () => {
                 longitude: restaurant.geometry.location.lng,
               }}
             >
-              <MapView.Callout>
+              <MapView.Callout
+                onPress={() =>
+                  navigation.navigate("RestaurantDetail", {
+                    restaurant,
+                  })
+                }
+              >
                 <MapCallout restaurant={restaurant} />
               </MapView.Callout>
             </MapView.Marker>
